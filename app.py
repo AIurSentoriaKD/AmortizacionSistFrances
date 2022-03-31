72597377463d81632d32d3a1eb8243a92a22c25f
Aiur <diegomactor13@gmail.com>
2022-03-31 13:35:44 -0500

    little fix

--- app.py
+++ app.py
@@ -16,22 +16,9 @@ def amortizacion(m, i, p):
         nuevosaldo = round(tabla[x][4]-amortiz, 2)
         tabla.append([x+1, renta, interes, amortiz, nuevosaldo])
         # print(renta,interes,amortiz,nuevosaldo)
-    
     return tabla
 
-def validarinputs(m, i, p):
     
-
-    if m < 1000 or m > 50000:
-        print("Error de m")
-        return render_template("./index.html", mensaje ="Valor del monto no valido")
-    if i < 0.01 or i > 0.04: 
-        print("Error de interes")
-        return render_template("./index.html", mensaje ="Valor del interes no valido")
-    if p < 6 or p > 24: 
-        print("Error de periodo")
-        return render_template("./index.html", mensaje ="Valor del periodo no valido")
-
 
 @app.route("/", methods=['GET'])
 def index():
@@ -43,19 +30,22 @@ def index():
         print(m, i, p)
 
         # validación de inputs
-        validarinputs(m,i,p)
+        if m < 1000 or m > 50000:
+            print("Error de monto")
+            return render_template("./index.html", mensaje ="Valor del monto no valido")
+        if i < 0.01 or i > 0.04: 
+            print("Error de interes")
+            return render_template("./index.html", mensaje ="Valor del interes no valido")
+        if p < 6 or p > 24: 
+            print("Error de periodo")
+            return render_template("./index.html", mensaje ="Valor del periodo no valido")
 
         # calculo de amortización a una tabla
         tabla = amortizacion(m, i, p)
-
         
         return render_template("./index.html", data = tabla)
     except:
         print("No hay parametros o uno es incorrecto")
         return render_template("./index.html")
-
-    
-    
-
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
